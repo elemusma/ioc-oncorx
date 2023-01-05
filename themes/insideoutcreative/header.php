1743a27b8e14850516f740d3ab8c308141cb09ed
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-05 10:19:49 -0700

    remove header from single php

--- themes/insideoutcreative/header.php
+++ themes/insideoutcreative/header.php
@@ -83,72 +83,74 @@ echo '</div>';
 
 echo '</header>';
 
-echo '<section class="hero position-relative">';
-if(is_front_page()) {
+if(!is_single()){
+    echo '<section class="hero position-relative">';
+    if(is_front_page()) {
 
-    $globalPlaceholderImg = get_field('global_placeholder_image','options');
-if(is_page()){
-if(has_post_thumbnail()){
-the_post_thumbnail('full', array('class' => 'w-100 h-100 bg-img position-absolute'));
-} else {
-echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute']);
+        $globalPlaceholderImg = get_field('global_placeholder_image','options');
+    if(is_page()){
+    if(has_post_thumbnail()){
+    the_post_thumbnail('full', array('class' => 'w-100 h-100 bg-img position-absolute'));
+    } else {
+    echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute']);
+    }
+    } else {
+    echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute']);
+    }
+
+    echo '<div class="hero-padding" style="padding:25px 0px;">';
+    // echo '<div class="position-relative">';
+    // echo '<div class="multiply overlay position-absolute w-100 h-100 bg-img"></div>';
+    // echo '<div class="position-relative">';
+    echo '<div class="container-fluid">';
+    echo '<div class="row">';
+    echo '<div class="col-12 pl-lg-0" data-aos="fade-right">';
+    echo '<h1 class="pt-3 pb-3 pl-lg-5 mb-0 text-accent">' . get_the_title() . '</h1>';
+
+    echo '<div class="divider mt-4 mb-4" style="width:100%;max-width:650px;"></div>';
+
+    echo '<div class="pl-lg-5">';
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+    the_content();
+    endwhile;
+    endif;
+    echo '</div>';
+
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    // echo '</div>';
+    // echo '</div>';
+    echo '</div>';
+    }
+
+
+
+    if(!is_front_page()) {
+    echo '<div class="container pt-5 pb-5 text-white text-center">';
+    echo '<div class="row">';
+    echo '<div class="col-md-12">';
+    if(is_page() || !is_front_page()){
+    echo '<h1 class="">' . get_the_title() . '</h1>';
+    } elseif(is_single()){
+    echo '<h1 class="">' . get_single_post_title() . '</h1>';
+    } elseif(is_author()){
+    echo '<h1 class="">Author: ' . get_the_author() . '</h1>';
+    } elseif(is_tag()){
+    echo '<h1 class="">' . get_single_tag_title() . '</h1>';
+    } elseif(is_category()){
+    echo '<h1 class="">' . get_single_cat_title() . '</h1>';
+    } elseif(is_archive()){
+    echo '<h1 class="">' . get_archive_title() . '</h1>';
+    }
+    elseif(!is_front_page() && is_home()){
+    echo '<h1 class="">' . get_the_title(133) . '</h1>';
+    }
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    }
+
+    echo '</section>';
 }
-} else {
-echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute']);
-}
-
-echo '<div class="hero-padding" style="padding:25px 0px;">';
-// echo '<div class="position-relative">';
-// echo '<div class="multiply overlay position-absolute w-100 h-100 bg-img"></div>';
-// echo '<div class="position-relative">';
-echo '<div class="container-fluid">';
-echo '<div class="row">';
-echo '<div class="col-12 pl-lg-0" data-aos="fade-right">';
-echo '<h1 class="pt-3 pb-3 pl-lg-5 mb-0 text-accent">' . get_the_title() . '</h1>';
-
-echo '<div class="divider mt-4 mb-4" style="width:100%;max-width:650px;"></div>';
-
-echo '<div class="pl-lg-5">';
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-the_content();
-endwhile;
-endif;
-echo '</div>';
-
-echo '</div>';
-echo '</div>';
-echo '</div>';
-// echo '</div>';
-// echo '</div>';
-echo '</div>';
-}
-
-
-
-if(!is_front_page()) {
-echo '<div class="container pt-5 pb-5 text-white text-center">';
-echo '<div class="row">';
-echo '<div class="col-md-12">';
-if(is_page() || !is_front_page()){
-echo '<h1 class="">' . get_the_title() . '</h1>';
-} elseif(is_single()){
-echo '<h1 class="">' . get_single_post_title() . '</h1>';
-} elseif(is_author()){
-echo '<h1 class="">Author: ' . get_the_author() . '</h1>';
-} elseif(is_tag()){
-echo '<h1 class="">' . get_single_tag_title() . '</h1>';
-} elseif(is_category()){
-echo '<h1 class="">' . get_single_cat_title() . '</h1>';
-} elseif(is_archive()){
-echo '<h1 class="">' . get_archive_title() . '</h1>';
-}
-elseif(!is_front_page() && is_home()){
-echo '<h1 class="">' . get_the_title(133) . '</h1>';
-}
-echo '</div>';
-echo '</div>';
-echo '</div>';
-}
-
-echo '</section>';
 ?>
